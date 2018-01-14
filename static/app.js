b8e61e6f62b21ec4ba48d630d702bc3cbc50721f
rubnvp <rubn.vp@gmail.com>
2018-01-14 22:04:20 +0100

    add cuisine facet in front

--- static/app.js
+++ static/app.js
@@ -9,6 +9,13 @@ var app = new Vue({
     data: function() {
         return {
             page: 0,
+            selectedFacets: {
+                cuisine: [],
+            },
+            facetCusine: [
+                {label: 'American (100)', value: 'American', type: 'cuisine'},
+                {label: 'Delicatessen (100)', value: 'Delicatessen', type: 'cuisine'},
+            ],
             restaurants: [],
             restaurantsCount: '',
         };
@@ -20,13 +27,33 @@ var app = new Vue({
         nextPage: function() {
             this.page++;
         },
+        facetClicked: function(facet) {
+            var facetList = this.selectedFacets[facet.type];
+            if (!facetList) return;
+
+            var facetIndex = facetList.indexOf(facet.value);
+            // add facet
+            if (facetIndex === -1 ) {
+                facetList.push(facet.value);
+            }
+            else { // remove facet
+                facetList.splice(facetIndex, 1);
+            }
+        },
+        isFacetSelected: function(facet) {
+            var facetList = this.selectedFacets[facet.type];
+            if (!facetList) return false;
+            return facetList.indexOf(facet.value) !== -1;
+        },
         fetchRestaurants: function() {
             var self = this;
             var options = {
                 params: {
                     page: this.page,
+                    cuisines: this.selectedFacets.cuisine.join(','),
                 }
             };
+            if (!options.params.cuisines.length) delete options.params.cuisines;
             return axios.get(API_ENDPOINT + '/restaurants/', options).then(function(response) {
                 self.restaurants = response.data;
             });
@@ -42,6 +69,7 @@ var app = new Vue({
         filters: function() { // computed property to watch
             return {
                 page: this.page,
+                cuisine: this.selectedFacets.cuisine,
             };
         },
     },
