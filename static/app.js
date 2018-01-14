8967566c76fd773d8d29572411adcfe287961c8d
rubnvp <rubn.vp@gmail.com>
2018-01-14 21:17:33 +0100

    added show restaurants list with pagination

--- static/app.js
+++ static/app.js
@@ -8,19 +8,51 @@ var app = new Vue({
     template: '#app',
     data: function() {
         return {
+            page: 0,
+            restaurants: [],
             restaurantsCount: '',
         };
     },
     methods: {
+        previousPage: function() {
+            this.page--;
+        },
+        nextPage: function() {
+            this.page++;
+        },
+        fetchRestaurants: function() {
+            var self = this;
+            var options = {
+                params: {
+                    page: this.page,
+                }
+            };
+            return axios.get(API_ENDPOINT + '/restaurants/', options).then(function(response) {
+                self.restaurants = response.data;
+            });
+        },
         fetchRestaurantsCount: function() {
             var self = this;
-            axios.get(API_ENDPOINT + '/restaurants/count').then(function(response) {
+            return axios.get(API_ENDPOINT + '/restaurants/count').then(function(response) {
                 self.restaurantsCount = response.data;
             });
         },
     },
+    computed: {
+        filters: function() { // computed property to watch
+            return {
+                page: this.page,
+            };
+        },
+    },
+    watch: {
+        filters: function() { // fetch restaurants every time filters change
+            this.fetchRestaurants();
+        },
+    },
     mounted: function() {
         this.fetchRestaurantsCount();
+        this.fetchRestaurants();
     }
 });
     
